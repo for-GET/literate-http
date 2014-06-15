a4ec1635ccd2f8dcad8a1db70f5515f875ae198a
Andrei Neculau <andrei.neculau@gmail.com>
2014-06-15 03:15:23 +0200

    add AST tests

--- custom.mk
+++ custom.mk
@@ -1,7 +1,20 @@
-.PHONY: all
+.PHONY: all test test/ast custom.mk
 
 all:
 	@$(MAKE) -f .coffee.mk/coffee.mk $@
 	echo "module.exports=$$(api-pegjs-source 'core/for-get/literate-http' 'litHTTP')" > lib/parse.js
 	echo "module.exports=$$(api-pegjs-source 'core/for-get/literate-http-curl' 'litHTTP')" > lib/parseCurl.js
 	echo "module.exports=$$(api-pegjs-source 'core/for-get/literate-http-apib' 'litHTTP')" > lib/parseApib.js
+
+test: test/ast
+
+test/ast:
+	for format in http lithttp apib curl curl-trace; do \
+		echo "Testing $${format}..." && \
+		(cat README.$${format}.md | bin/to-ast.coffee $${format} && echo -ne "\n") > test/README.$${format}.json && \
+		[[ -f test/expected.README.$${format}.json ]] && diff test/README.$${format}.json test/expected.README.$${format}.json || diff test/README.$${format}.json README.json && \
+		:; \
+	done
+
+custom.mk:
+	@:
